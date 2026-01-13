cafc94c7d78325f7f4f6a6204d1e3254f15dded5
Andriy Zavada <andriy.zavada@tiot.jp>
2026-01-13 23:20:44 +0200

    make rel for rc0

---
+++ rel/out/bin/rctl_ssh.py
@@ -0,0 +1,97 @@
+## Copyright (c) 2026 TI Tokyo    All Rights Reserved.
+##
+## This file is provided to you under the Apache License,
+## Version 2.0 (the "License"); you may not use this file
+## except in compliance with the License.  You may obtain
+## a copy of the License at
+##
+##   http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing,
+## software distributed under the License is distributed on an
+## "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+## KIND, either express or implied.  See the License for the
+## specific language governing permissions and limitations
+## under the License.
+
+import os, time, uuid
+import logging
+import subprocess
+import rctl_globals
+
+def make_and_exec(url, user, key, template_body, params):
+    if key:
+        idf = open(rctl_globals.DATADIR + "/_key_", mode = 'w+')
+        idf.write(key['body'])
+        idf.close()
+        os.chmod(idf.name, 0o600)
+        idf_name = idf.name
+    else:
+        idf_name = None
+
+    scriptf = open(rctl_globals.DATADIR + "/_script_", mode = 'w+')
+    pp = []
+    for p in params:
+        pp += [p+"=\""+params[p]+"\""]
+    pstr = "#!/bin/sh\nexport " + " ".join(pp) + "\n"
+
+    scriptf.write(pstr + template_body)
+    scriptf.close()
+
+    _scp(url, user, idf_name, scriptf.name)
+
+    res = _ssh_exec(url, user, idf_name, scriptf.name, params)
+
+    return res
+
+def cleanup(idf, scriptf):
+    if idf is not None:
+        os.unlink(idf)
+    os.unlink(scriptf)
+
+
+def _option_i(f):
+    if f:
+        return ["-i", f]
+    else:
+        return []
+
+_option_o = ["-o", "KbdInteractiveAuthentication=no",
+             "-o", "PasswordAuthentication=no",
+             "-o", "UpdateHostKeys=no",
+             "-o", "StrictHostKeyChecking=no"]
+
+def _scp(url, user, idf, f):
+    logging.info("copying script %s to %s as %s (using key %s)", f, url, user, idf)
+    try:
+        p = subprocess.run(["scp"] + _option_i(idf) + _option_o +
+                           [f, user+"@"+url+":"],
+                           capture_output = True,
+                           encoding ='utf8',
+                           timeout = 15)
+        if p.returncode != 0:
+            raise rctl_globals.RctlException(404, "scp failed ({}): {}".format(p.returncode, p.stderr))
+    except subprocess.TimeoutExpired:
+        raise rctl_globals.RctlException(408, "Host unreachable")
+
+def _ssh_exec(url, user, idf_name, scriptf_name, params):
+    logging.info("executing script %s on %s as %s (using key %s)", scriptf_name, url, user, idf_name)
+    ssh_options = _option_i(idf_name) + _option_o
+    subprocess.run(["ssh"] + ssh_options + [user+"@"+url, "chmod", "+x", os.path.basename(scriptf_name)])
+    p = subprocess.Popen(["ssh"] + ssh_options +
+                         [user+"@"+url, "./"+os.path.basename(scriptf_name)],
+                         encoding ='utf8',
+                         stdout = subprocess.PIPE,
+                         stderr = subprocess.STDOUT,
+                         text = True)
+
+    session_id = str(uuid.uuid4())
+    session = {
+        "process": p,
+        "sent_bytes": 0,
+        "script_file": scriptf_name,
+        "id_file": idf_name
+    }
+    rctl_globals.ACTIVE_SSH_SESSIONS |= {session_id: session}
+
+    return {"session_id": session_id}
